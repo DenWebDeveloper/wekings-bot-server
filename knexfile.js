9bb1bb9ffc6d3592b7fbea77a28b5134e5d6ca09
Denys <denv928@gmail.com>
2019-01-22 21:26:46 +0200

    evn fix

--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,7 @@ module.exports = {
 
     production: {
         client: 'pg',
-        connection: process.env.DATABASE_URI_SSL,
+        connection: 'postgres://lzoywqgzuekvju:67756c347ae85c2098ee21d0e486b5931ad593932fe42e0e77ca5d8a2f42a51d@ec2-54-247-82-210.eu-west-1.compute.amazonaws.com:5432/d3ehsgetm3kals?ssl=true',
         migrations: {
             directory: './server/database/migrations'
         },
