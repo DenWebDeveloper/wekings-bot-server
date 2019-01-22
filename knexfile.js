59059625dcec0084268cb8cdff033e2bf87363cd
Denys <denv928@gmail.com>
2019-01-22 20:24:26 +0200

    preparing to deploy

--- knexfile.js
+++ knexfile.js
@@ -16,18 +16,13 @@ module.exports = {
     },
 
     production: {
-        client: 'postgresql',
-        connection: {
-            database: 'my_db',
-            user: 'username',
-            password: 'password'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
+        client: 'pg',
+        connection: process.env.DATABASE_URI_SSL,
         migrations: {
-            tableName: 'knex_migrations'
+            directory: './server/database/migrations'
+        },
+        seeds: {
+            directory: './server/database/seeds'
         }
     }
 }
